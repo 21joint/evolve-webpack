c130cbdc8752039d36c45ccaaeb082eb3cfa4c17
SAMVELHARUTYUNYAN <samvelharutyunyan777@gmail.com>
2018-07-19 10:13:16 +0400

    autopushed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-
 import './assets/scss/index.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'html-loader!index.html';
+import 'scripts/evolve';
---
+++ src/scripts/evolve.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  $('.evlv-menu--toggler').click(function (e) {
+    e.preventDefault();
+    $('body').toggleClass('evlv-dashmenu--open');
+  });
+
+
+});
+
+
