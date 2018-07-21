41ec4c299b39ed9a2a7255a56aaf253bc6740f41
SAMVELHARUTYUNYAN <samvelharutyunyan777@gmail.com>
2018-07-21 08:47:36 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './assets/scss/index.scss';
+import './assets/icomoon/style.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'html-loader!index.html';
 import 'scripts/evolve';
