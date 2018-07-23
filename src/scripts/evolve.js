cd08e0b59acfdc5d5c28eb8a4b6b82ed2b99cb23
SAMVELHARUTYUNYAN <samvelharutyunyan777@gmail.com>
2018-07-23 08:47:29 +0400

    updates

--- src/scripts/evolve.js
+++ src/scripts/evolve.js
@@ -6,3 +6,46 @@ $(document).ready(function () {
   });
 
 });
+
+
+var buttonCheked  = function(callback){
+
+
+  $(".bwdash-btn--rounded").on("click", function(){
+    callback(true);
+  });
+
+};
+
+buttonCheked(function(cheked){
+  if(cheked){
+    //Acciones si el usuario confirma
+    $(".bwdash-btn--rounded").parents(".bwdash-list--item").addClass("cheked") && $(".dining-redeem-confirm--btn").css({"background-color": "#b8ea86"});
+  }else{
+    //Acciones si el usuario no confirma
+  }
+});
+
+var modalConfirm = function(callback){
+
+
+  $("#modal-btn-yes").on("click", function(){
+    callback(true);
+    $("#diningConfirmModalCenter").modal('hide');
+  });
+
+  $("#modal-btn-no").on("click", function(){
+    callback(false);
+    $("#diningConfirmModalCenter").modal('hide');
+  });
+};
+
+modalConfirm(function(confirm){
+  if(confirm){
+    //Acciones si el usuario confirma
+    $(".bwdash-list--item.cheked").css({"opacity": "0.3"});
+  }else{
+    //Acciones si el usuario no confirma
+    $("#diningConfirmModalCenter").modal('hide');
+  }
+});
